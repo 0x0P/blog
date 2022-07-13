2cc29bfb74cc8564ca49f7cc353a34b3d49294bd
PCNT <zero1105x@outlook.com>
2022-07-14 04:21:08 +0900

    blog
    Signed-off-by: PCNT <zero1105x@outlook.com>

--- components/post.tsx
+++ components/post.tsx
@@ -6,40 +6,39 @@ type propsTypes = {
   img,title,id,date,about: string;
   tags: Array<string>;
 };
-const BlogPost : NextPage<propsTypes> = (props) => {
-  let img = props.img
-  if(img === undefined) {
-    img = "/"
+function BlogPost(props): JSX.Element {
+  let img = props.img;
+  if (img === undefined) {
+    img = "/";
   }
   return (
     <div>
       <div data-aos="fade-up" className={`cursor-pointer bg-white dark:bg-gray h-min rounded-xl w-full md:max-w-xs hover:drop-shadow-lg  ease-in-out duration-150`}>
-      <div className="p-5 pb-0 flex flex-row gap-2 flex-wrap">
-          {props.tags?.map((tag) => ( 
-            <Link href={`/tags/${tag}`} passHref> 
-                <a>
-                  <div className="bg-indigo-400/20 hover:bg-indigo-300/60  dark:bg-indigo-800/20 dark:hover:bg-indigo-800/60 text-indigo-500  duration-300 w-fit h-min pl-1 pr-1 rounded-md">{`#${tag}`}</div>
-                </a>
-              </Link>
+        <div className="p-5 pb-0 flex flex-row gap-2 flex-wrap">
+          {props.tags?.map((tag) => (
+            <Link href={`/tags/${tag}`} passHref>
+              <a>
+                <div className="bg-indigo-400/20 hover:bg-indigo-300/60  dark:bg-indigo-800/20 dark:hover:bg-indigo-800/60 text-indigo-500  duration-300 w-fit h-min pl-1 pr-1 rounded-md">{`#${tag}`}</div>
+              </a>
+            </Link>
           ))}
         </div>
-      <Link href={`/posts/${props.id}`} passHref> 
-        <a className="mt-3 w-min">
+        <Link href={`/posts/${props.id}`} passHref>
+          <a className="mt-3 w-min">
             <div className="p-5 pt-1 pb-3">
-            <div className={`font-bold text-2xl break-words`}>{props.title}</div>
-            <h1 className="font-thin text-sm w-fit">{props.date}</h1>
-            <div className={`font-light`}>{props.about}</div>
+              <div className={`font-bold text-2xl break-words`}>{props.title}</div>
+              <h1 className="font-thin text-sm w-fit">{props.date}</h1>
+              <div className={`font-light`}>{props.about}</div>
             </div>
             <Image
-            className="object-contain w-full rounded-b-xl"
+              className="object-contain w-full rounded-b-xl"
               src={`${img}`}
-              alt=""
-            />
+              alt="" />
           </a>
         </Link>
-          </div>
       </div>
+    </div>
   );
-};
+}
 
 export default BlogPost;
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react'
 import { changeTitle } from '../../lib/ct'
 import ScrollProgress from 'scrollprogress';
 import Image from "next/future/image";
+import Link from 'next/link'
 
 
 export default function Post({
@@ -48,9 +49,13 @@ let style = {
       <h1 className={`text-5xl font-extrabold m-5 mb-0 mt-10`}>{postData.title}</h1>
       <h1 className={`text-sm font-thin m-5 `}>{postData.date}</h1>
       <div className="m-5 mt-3  flex flex-row gap-2 flex-wrap">
-          {postData.tags.map((tag) => ( 
-            <div className="bg-indigo-400/20 hover:bg-indigo-300/60  dark:bg-indigo-800/20 dark:hover:bg-indigo-800/60 text-indigo-500  duration-300 w-fit h-min pl-1 pr-1 rounded-md">{`#${tag}`}</div>
-            ))}
+      {postData.tags?.map((tag) => (
+            <Link href={`/tags/${tag}`} passHref>
+              <a>
+                <div className="bg-indigo-400/20 hover:bg-indigo-300/60  dark:bg-indigo-800/20 dark:hover:bg-indigo-800/60 text-indigo-500  duration-300 w-fit h-min pl-1 pr-1 rounded-md">{`#${tag}`}</div>
+              </a>
+            </Link>
+          ))}
         </div>
         <div>
         </div>
