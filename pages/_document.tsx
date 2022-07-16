b0e402c6646aeb403b8bcd1a199a8ef836931b69
PCNT <zero1105x@outlook.com>
2022-07-16 14:13:34 +0900

    blog
    Signed-off-by: PCNT <zero1105x@outlook.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head> 
       <meta property="og:site_name" content={metadata.author} />
       <meta content={metadata.description} name="description" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7519087055998524"
+     crossOrigin="anonymous"></script>
       </Head>
       <body>
         <Main />
